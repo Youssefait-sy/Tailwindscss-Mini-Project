faf971c3d9ea7c28394dbec28af9408849b08e7e
Youssef <youssef19@gmail.com>
2025-01-16 18:07:59 +0100

    our project are ready

---
+++ dist/Js/main.js
@@ -0,0 +1,7 @@
+
+const mobileBtn = document.getElementById('mobile-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileBtn.addEventListener('click',() => {
+    mobileMenu.classList.toggle('hidden');
+})
